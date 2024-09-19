8f0b2139093a6571929a6659065eaf45bffacd44
Julián Loaiza <andres.loaiza@est.iudigital.edu.co>
2024-09-18 20:36:06 -0500

    Cree mi rama para realizar la actualización de una clase

--- src/IUDIGITAL/Departamento.java
+++ src/IUDIGITAL/Departamento.java
@@ -1,4 +1,15 @@
 package IUDIGITAL;
 
 public class Departamento {
+    //atributos
+    private String nombre;
+    private String ubicacion;
+    private String telefono;
+    private String correo;
+    private String jefe;
+    private String extension;
+    private String horario;
+    private String descripcion;
+    private String mision;
+    private String vision;
 }
