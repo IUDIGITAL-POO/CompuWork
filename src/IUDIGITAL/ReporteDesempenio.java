82afd3307c58f7b2c35b9772c5d445fb91f911c4
MarianaA-A <mariana.agudelo@est.iudigital.edu.co>
2024-09-20 04:36:36 -0500

    Subiendo atributos de la clase ReporteDesempenio

--- src/IUDIGITAL/ReporteDesempenio.java
+++ src/IUDIGITAL/ReporteDesempenio.java
@@ -1,4 +1,47 @@
 package IUDIGITAL;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ReporteDesempenio {
+
+    private int idReporte;
+    private Empleado empleado;
+    private Departamento departamento;
+    private Date fechaReporte;
+    private String metricas;
+
+    public ReporteDesempenio(int idReporte, Empleado empleado, Departamento departamento, Date fechaReporte, String metricas) {
+        this.idReporte = idReporte;
+        this.empleado = empleado;
+        this.departamento = departamento;
+        this.fechaReporte = fechaReporte;
+        this.metricas = metricas;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    public Date getFechaReporte() {
+        return fechaReporte;
+    }
+    public void setFechaReporte(Date fechaReporte) {
+        this.fechaReporte = fechaReporte;
+    }
+    public String getMetricas() {
+        return metricas;
+    }
+    public void setMetricas(String metricas) {
+        this.metricas = metricas;
+    }
 }
